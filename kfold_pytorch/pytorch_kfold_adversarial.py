8846fbc9786e51665821d72ee67c2125255f837f
joshua <joshuadarecullen@protonmail.com>
2022-09-08 16:56:21 +0100

    finally edits

--- kfold_pytorch/pytorch_kfold_adversarial.py
+++ kfold_pytorch/pytorch_kfold_adversarial.py
@@ -768,18 +768,3 @@ class Pytorch_KFold_Adversary:
 
     def set_model(self, state_dict):
         self.model.load_state_dict(state_dict)
-
-# Accumulates metrics i wanna know in a smart form
-class Accumulator:
-    """For accumulating sums over `n` variables."""
-    def __init__(self, n):
-        self.data = [0.0] * n
-
-    def add(self, *args):
-        self.data = [a + float(b) for a, b in zip(self.data, args)]
-
-    def reset(self):
-        self.data = [0.0] * len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx]
